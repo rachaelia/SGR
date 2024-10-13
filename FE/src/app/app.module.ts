c2ad14a09eabdce544761f4554c09ca75d466fe1
rachaelia <rachele.elia14@gmail.com>
2024-10-14 00:38:26 +0200

    style fixes

--- FE/src/app/app.module.ts
+++ FE/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { MatInputModule } from '@angular/material/input';
 import { SentimentService } from './sentiment.service';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     CommonModule,
-    FormsModule,
     MatInputModule,
-    BrowserModule,
-    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatButtonModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatIconModule
   ],
   providers: [SentimentService],
   bootstrap: [AppComponent],
